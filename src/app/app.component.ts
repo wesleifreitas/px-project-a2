977fcdea8e6f27be01bb8cb90576008392466445
Weslei Freitas <weslei.rfreitas@gmail.com>
2017-07-25 11:21:26 -0300

    feat(init): iniciar estudos — angular io

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+import '../assets/app.css';
+
+@Component({
+  moduleId: module.id.toString(),
+  selector: 'app-root',
+  templateUrl: 'app.component.html'
+})
+
+export class AppComponent { }
